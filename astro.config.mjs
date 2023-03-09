1faf02e7f449b210935e13cfe48d004223c59ec4
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-03-09 08:26:46 +0530

    I keep getting more and more scared at the size of this

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,14 @@ import preact from "@astrojs/preact";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import solidJs from "@astrojs/solid-js";
+import astroI18next from "astro-i18next";
+
 
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://example.com',
   integrations: [
+    astroI18next(), // a11y
     mdx(), sitemap(), // Basic support
     react(), preact(), solidJs(), // JSX support
     vue(), svelte(), // Template support
