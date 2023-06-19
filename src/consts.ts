04858008e41861aff11cc8f54f93a9d30fb3f307
Apeksha Meharkar <18apeksha.m@gmail.com>
2023-06-19 16:33:55 +0530

    changes made till now

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Postcards for Equality';
-export const SITE_DESCRIPTION = 'A blog by The Barabari Project!';
+export const SITE_TITLE = 'The Barabari Blog';
+export const SITE_DESCRIPTION = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptatum modi. Animi repellat accusamus facere tempora labore alias. Accusantium provident unde exercitationem placeat officia ullam!';
